257741a760b8b02edf4217eec080ddcf38a94c5e
qianfeng <qianfeng@001-zhangyutao-3.local>
2014-07-30 10:20:05 +0800

    Initial Commit

---
+++ iCartooniGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iCartooniGame
+//
+//  Created by qianfeng on 14-7-30.
+//  Copyright (c) 2014年 michaelalei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
